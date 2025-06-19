3e593e7f151362286fafcf2685fa765051302c85
Charan007x <saicharangottipalli@gmail.com>
2025-06-19 22:12:34 +0530

    Create LongestSubStringWithoutRepeatingCharactersV2.java

---
+++ SlidingWindow/LongestSubStringWithoutRepeatingCharactersV2.java
@@ -0,0 +1,23 @@
+//Method 2
+//LongestSubStringWithoutRepeatingCharacters
+import java.util.*;
+public class lfg{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String s = sc.next();
+        int l=0,h=0,curr=0,max=0;
+        HashSet<Character> set = new HashSet<>();
+        while(h<n){
+            char ch = s.charAt(h);
+            while(set.contains(ch)){
+                set.remove(s.charAt(l));
+                l++;
+            }
+            set.add(ch);
+            max=Math.max(h-l+1,max);
+            h++;
+        }
+        System.out.println(max);
+    }
+}
