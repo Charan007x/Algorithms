9837c5334e60c7eaf209a101a9f698201ee37d33
Charan007x <saicharangottipalli@gmail.com>
2025-06-26 22:23:08 +0530

    Create Optimized.java

---
+++ DynamicProgramming/CountVowelsInSubString/Optimized.java
@@ -0,0 +1,23 @@
+// Space Optimised Approach
+import java.util.*;
+public class New{
+    public static boolean vow(char ch){
+        if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u') return true;
+        return false;
+    }
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        int n = s.length();
+        char ch = s.charAt(0);
+        int total=0,curr=0,prev=0;
+        for(int i=0;i<n;i++){
+            ch=s.charAt(i);
+            if(vow(ch)) curr=prev+i+1; 
+            else curr=prev;
+            total+=curr;
+            prev=curr;
+        }
+        System.out.println(total);
+    }
+}
