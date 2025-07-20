34332da844aa2aca0f46ed2c34c59f5a3cd64f02
Sai Charan Gottipalli <saicharangottipalli@gmail.com>
2025-07-20 19:00:52 +0530

    Create BFS.java

---
+++ BinaryTrees/BFS.java
@@ -0,0 +1,40 @@
+// Breadth First Search
+import java.util.*;
+public class lfg{
+    public static class Node{
+        int val;
+        Node left,right;
+        Node(int val){
+            this.val=val;
+            this.left=null;
+            this.right=null;
+        }
+    }
+    public static void display(Node root){
+        Queue<Node> q = new LinkedList<>();
+        if(root!=null) q.add(root);
+        while(!q.isEmpty()){
+            Node temp = q.poll();
+            System.out.print(temp.val+" ");
+            if(temp.left!=null)q.add(temp.left);
+            if(temp.right!=null)q.add(temp.right);
+        }
+    }
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        Node root = new Node(1);
+        Node a = new Node(2);
+        Node b = new Node(3);
+        Node c = new Node(4);
+        Node d = new Node(5);
+        Node e = new Node(6);
+        Node f = new Node(7);
+        root.left=a;
+        root.right=b;
+        a.left=c;
+        a.right=d;
+        b.left=e;
+        b.right=f;
+        display(root);
+    }
+}
