ca068c943a25253e94002cea4b816b72a25a644a
Sai Charan Gottipalli <saicharangottipalli@gmail.com>
2025-07-20 15:21:16 +0530

    Create PreOrderTraversal.java

---
+++ BinaryTrees/PreOrderTraversal.java
@@ -0,0 +1,36 @@
+// PreOrder Traversal
+import java.util.*;
+public class lfg{
+    public static class Node{
+        int val;
+        Node left,right;
+        Node(int val){
+            this.val=val;
+            this.right=null;
+            this.left=null;
+        }
+    }
+    public static void display(Node root){
+        if(root==null) return;
+        System.out.println(root.val);
+        display(root.left);
+        display(root.right);
+    }
+    public static void main(String[]args){
+        Scanner sc= new Scanner(System.in);
+        Node root = new Node(1);
+        Node a = new Node(2);
+        Node b = new Node(3);
+        Node c = new Node(4);
+        Node d = new Node(5);
+        Node e = new Node(6);
+        Node f = new Node(7);
+        root.left=a;
+        root.right=b;
+        a.left=c;
+        a.right=d;
+        b.left=e;
+        b.right=f;
+        display(root);
+    }
+}
