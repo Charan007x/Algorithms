9d53994f738b28078ab37194359546db5b7f475c
Sai Charan Gottipalli <saicharangottipalli@gmail.com>
2025-07-20 15:37:42 +0530

    Create PostOrderTraversal.java

---
+++ BinaryTrees/PostOrderTraversal.java
@@ -0,0 +1,36 @@
+// PostOrder Traversal
+import java.util.*;
+public class lfg{
+    public static class Node{
+        int val;
+        Node left,right;
+        Node(int val){
+            this.val=val;
+            this.left=null;
+            this.right=null;
+        }
+    }
+    public static void display(Node root){
+        if(root==null) return;
+        display(root.left);
+        display(root.right);
+        System.out.println(root.val);
+    }
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        Node root = new Node(1);
+        Node a = new Node(2);
+        Node b = new Node(3);
+        Node c = new Node(4);
+        Node d = new Node(5);
+        Node e = new Node(6);
+        Node f = new Node(7);
+        root.left=a;
+        root.right=b;
+        a.left=c;
+        a.right=d;
+        b.left=e;
+        b.right=f;
+        display(root);
+    }
+}
