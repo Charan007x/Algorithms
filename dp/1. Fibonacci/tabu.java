7f8da02a3d50b5ef425e79dcc968cd57edf205f6
Sai Charan Gottipalli <saicharangottipalli@gmail.com>
2025-09-13 17:14:24 +0530

    Create tabu.java

---
+++ dp/1. Fibonacci/tabu.java
@@ -0,0 +1,9 @@
+// TC - o(n), SC-o(n) 
+public static int fun(int n){
+        int dp[]=new int[n+1];
+        dp[0]=0;dp[1]=1;dp[2]=1;
+        for(int i=3;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
