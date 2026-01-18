62c9a71767a93285b8b726fbe2aa590b56b286f1
Sai Charan Gottipalli <saicharangottipalli@gmail.com>
2026-01-18 20:32:38 +0530

    Refactor next smaller element calculation
    Refactor next smaller element logic to improve clarity and efficiency.

--- Stacks/NextSmallerElement.java
+++ Stacks/NextSmallerElement.java
@@ -3,19 +3,15 @@ public class New{
     public static void main(String[]args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int a[] = new int[n];
+        int a[]= new int[n];
         for(int i=0;i<n;i++) a[i]=sc.nextInt();
-        int nse[]=new int[n];
         Stack<Integer> st = new Stack<>();
+        int nse[]=new int[n];
+        Arrays.fill(nse,-1);
         for(int i=n-1;i>=0;i--){
-            while(!st.isEmpty()&&a[i]<=st.peek()) st.pop();
-            if(!st.isEmpty()&&a[i]>st.peek()){
-                nse[i]=st.peek();
+            while(!st.isEmpty()&&st.peek()>=a[i]) st.pop();
+            if(!st.isEmpty()) nse[i]=st.peek();
                 st.push(a[i]);
-            }else if(st.isEmpty()){
-                nse[i]=-1;
-            st.push(a[i]);
-            }
         }
         for(int x:nse) System.out.print(x+" ");
     }
