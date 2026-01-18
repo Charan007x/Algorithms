fb3921808c3b02416a895e6ebe6df93b34b80699
Sai Charan Gottipalli <saicharangottipalli@gmail.com>
2026-01-18 22:47:12 +0530

    Update InfixToPostfix.java

--- Stacks/InfixToPostfix.java
+++ Stacks/InfixToPostfix.java
@@ -1,28 +1,24 @@
 import java.util.*;
 public class In_Po {
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
-        String s="a+b*c/d-e";
-        int n=s.length();
-        Stack<Character> st=new Stack<>();
+       String s="a+b*c";
         StringBuilder sb=new StringBuilder();
+        Stack<Character> st=new Stack<>();
+        int n=s.length();
         for(int i=0;i<n;i++){
             char ch=s.charAt(i);
             if(ch>='a'&&ch<='z') sb.append(ch);
+            else if(ch=='(') st.push(ch);
+            else if(ch==')'){
+                while(st.size()>0 && st.peek()!='(') sb.append(st.pop());
+                st.pop();
+            }
             else{
-                if(st.isEmpty()) st.push(ch);
-                else if(ch=='(') st.push(ch);
-                else if(ch==')'){
-                    while(st.peek()!='(') sb.append(st.pop());
-                    st.pop();
-                }
-                else{
-                    while(st.size()>0&&priority(st.peek())>=priority(ch)) sb.append(st.pop());
-                    st.push(ch);
-                }
+                while(st.size()>0&&pri(st.peek())>=pri(ch)) sb.append(st.pop());
+                st.push(ch);
             }
         }
-        while(st.size()!=0) sb.append(st.pop());
+        while(!st.isEmpty()) sb.append(st.pop());
         System.out.println(sb.toString());
     }
     public static int priority(char ch){
