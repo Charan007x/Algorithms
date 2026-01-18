e531d424d5d954adc1abef306167ef8ce5c9e8e2
Sai Charan Gottipalli <saicharangottipalli@gmail.com>
2026-01-18 20:47:49 +0530

    Refactor PreviousGreaterElement to use static array

--- Stacks/PreviousGreaterElement.java
+++ Stacks/PreviousGreaterElement.java
@@ -1,22 +1,17 @@
 import java.util.*;
 public class New{
     public static void main(String[]args){
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int a[]= new int[n];
-        for(int i=0;i<n;i++) a[i]=sc.nextInt();
-        int pge[]= new int[n];
-        Stack<Integer> st = new Stack<>();
+       Stack<Integer> st = new Stack<>();
+        int a[]={1,3,2,4};
+        int n=a.length;
+        int pge[]=new int[n];
+        Arrays.fill(pge,-1);
         for(int i=0;i<n;i++){
-            while(!st.isEmpty()&&a[i]>=st.peek()) st.pop();
-            if(!st.isEmpty()&&a[i]<st.peek()){
-                pge[i]=st.peek();
-                st.push(a[i]);
-            }else if(st.isEmpty()){
-                pge[i]=-1;
-                st.push(a[i]);
-            }
+            while(st.size()>0&&st.peek()<=a[i]) st.pop();
+            if(st.size()>0) pge[i]=st.peek();
+            st.push(a[i]);
         }
         for(int x:pge) System.out.print(x+" ");
+        System.out.println();
     }
 }
