2fa1227e03e0a50276768501851d6f4e74a39185
Sai Charan Gottipalli <saicharangottipalli@gmail.com>
2026-01-18 20:30:09 +0530

    Refactor NextGreaterElement.java logic
    Refactor NextGreaterElement logic to initialize nge array and simplify conditions.

--- Stacks/NextGreaterElement.java
+++ Stacks/NextGreaterElement.java
@@ -7,17 +7,11 @@ public class New{
         for(int i=0;i<n;i++) a[i]=sc.nextInt();
         Stack<Integer> st = new Stack<>();
         int nge[]=new int[n];
-        nge[n-1]=-1;
-        st.push(a[n-1]);
-        for(int i=n-2;i>=0;i--){
+        Arrays.fill)(nge,-1);
+        for(int i=n-1;i>=0;i--){
             while(!st.isEmpty()&&st.peek()<=a[i]) st.pop();
-            if(!st.isEmpty()&&st.peek()>a[i]){
-                nge[i]=st.peek();
+            if(!st.isEmpty()) nge[i]=st.peek();
                 st.push(a[i]);
-            }else if(st.isEmpty()){
-                nge[i]=-1;
-                st.push(a[i]);
-            }
         }
         for(int x:nge) System.out.print(x+" ");
     }
